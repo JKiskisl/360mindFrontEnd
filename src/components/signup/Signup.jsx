fc9e603374b27953cba2611bd49e2f19057a9aeb
JKiskisl <smicscl@gmail.com>
2023-07-29 17:14:39 +0300

    add signup screen

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,6 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./signup.css";
 
-const Signup = () => {};
+const Signup = () => {
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    const fullName = e.target.fullName.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log("Full Name:", fullName);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="signup-container">
+      <form onSubmit={handleSignup}>
+        <div className="form-group">
+          <label htmlFor="fullName">Full Name</label>
+          <input type="text" id="fullName" name="fullName" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
+    </div>
+  );
+};
 
 export default Signup;
